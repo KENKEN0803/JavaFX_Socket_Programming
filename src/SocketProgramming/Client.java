b9bd6f2499ea52ec09a25055c74a475ef838e3c2
KENKEN0803 <blackflamekr@gmail.com>
2021-09-03 17:33:30 +0900

    자바 채팅 프로그램 만들기

--- src/SocketProgramming/Client.java
+++ src/SocketProgramming/Client.java
@@ -1,141 +1,294 @@
-//package SocketProgramming;
-//
-//import javafx.application.Application;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.TextArea;
-//import javafx.scene.control.TextField;
-//import javafx.scene.layout.VBox;
-//import javafx.stage.Stage;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.OutputStream;
-//import java.net.InetAddress;
-//import java.net.InetSocketAddress;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//import java.nio.charset.StandardCharsets;
-//import java.util.ArrayList;
-//
-//
-//class ServerThreadC extends Thread {
-//    Socket ss;
-//    Client client;
-//
-//    ServerThreadC(Socket ss, Client client) {
-//        this.ss = ss;
-//        this.client = client;
-//    }
-//
-//    @Override
-//    public void run() {
-//        InputStream inputStream = null;
-//
-//        while (true) {
-//            try {
-//                ArrayList<String> list = new ArrayList<String>();
-//
-//                inputStream = ss.getInputStream();
-//                byte[] data = new byte[512];
-//                int size = inputStream.read(data);//블로킹
-//                String inputData = new String(data, 0, size, StandardCharsets.UTF_8);
-//                String clientAddress = ss.getInetAddress().getHostName();
-//                client.textArea.appendText(inputData);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//}
-//
-//
-//class ConnectThreadC extends Thread {
-//    Client client;
-//
-//    ConnectThreadC(Client client) {
-//        this.client = client;
-//    }
-//
-//    @Override
-//    public void run() {
-//        try {
-//            ServerSocket mss = new ServerSocket();
-//            int port = 5001;
-//            client.textArea.appendText("소캣 생성 완료\n");
-//            mss.bind(new InetSocketAddress("115.22.10.42", port));
-//            client.textArea.appendText("바인딩 완료\n" + port + "번 포트에서 서버 응답 대기중...\n");
-//            while (true) {
-//                Socket ss = mss.accept(); // 블로킹
-//                new ServerThreadC(ss, client).start();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-//
-//
-//class ClientThread extends Thread {
-//    Client client;
-//
-//    ClientThread(Client client) {
-//        this.client = client;
-//    }
-//
-//    @Override
-//    public void run() {
-//        try {
-//            Socket cs = new Socket();
-//
-//            String ServerIp = "115.22.10.42";
-//            int port = 5001;
-//            cs.connect(new InetSocketAddress(ServerIp, port));
-//            OutputStream outputStream = cs.getOutputStream();
-//
-//            String input = client.textField.getText(); // 텍스트필드에서 텍스트 가져옴
-//            byte[] data = input.getBytes(StandardCharsets.UTF_8); // 가져온 데이터를 배열에 담음
-//            outputStream.write(data); // 배열을 아웃풋스트림으로 전송
-//            System.out.println(input + "전송 완료"); // 콘솔에 출력
-//            client.textField.setText(""); // 텍스트필드를 공백으로 돌림
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-//
-//public class Client extends Application {
-//    Button btn1 = new Button("서버에 접속하기");
-//    Button btn2 = new Button("---");
-//    TextArea textArea = new TextArea();
-//    TextField textField = new TextField();
-//
-//    @Override
-//    public void start(Stage arg0) throws Exception {
-//        VBox root = new VBox(); // root 최상단
-//        root.setPrefSize(400, 300); // 창의 가로길이 세로길이 a설정.
-//        root.setSpacing(15); // 사이 간격을 띄운다.
-//
-//        // -----------------------------------------------------------------------
-//
-//        btn1.setOnAction(actionEvent -> {
-//            new ConnectThreadC(this).start();
-//        });
-//
-//        textField.setOnAction(actionEvent -> {
-//            new ClientThread(this).start();
-//        });
-//
-//        // -----------------------------------------------------------------------
-//        root.getChildren().addAll(btn1, btn2, textArea, textField); // 한꺼번에 등록시키는 addAll()
-//        Scene scene = new Scene(root); // scene : 한장면을 그리기위한 바탕.
-//        arg0.setScene(scene);
-//        arg0.setTitle("클라이언트");// 제목
-//        arg0.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println("클라이언트 스타트");
-//        launch();
-//    }
-//}
+package SocketProgramming;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+
+class ChatThread2 extends Thread {
+    Socket socket;
+    Client client;
+    String nickName;
+
+    ChatThread2(Socket socket, Client client, String nickName) {
+        this.socket = socket;
+        this.client = client;
+        this.nickName = nickName;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream inputstream = socket.getInputStream();
+            while (true) {
+                byte[] data = new byte[512];
+                int size = inputstream.read(data);
+                String s = new String(data, 0, size, StandardCharsets.UTF_8);
+                System.out.println(s + " 서버에서 데이터 받음");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class Client extends Application {
+    Socket cs;
+    VBox root = new VBox();
+    HBox hBox1 = new HBox();
+    HBox hBox2 = new HBox();
+    HBox hBox3 = new HBox();
+    HBox hBox4 = new HBox();
+    TextArea textArea = new TextArea();
+    TextField textField = new TextField();
+    TextField textField1 = new TextField();
+    TextField textField2 = new TextField();
+    TextField textField3 = new TextField();
+    Button btn1 = new Button("서버 연결!");
+    Button btn2 = new Button("방 나가기");
+    Button btn3 = new Button("전송");
+    Button btn4 = new Button("창닫기");
+    Label label1 = new Label();
+    Label label2 = new Label();
+    Label label3 = new Label();
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        root.setPrefSize(500, 400); // 창 크기
+        root.setSpacing(10);
+        root.setPadding(new Insets(15));
+        root.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
+
+        btn1.setPrefSize(100, 30);
+        btn2.setPrefSize(100, 30);
+        btn4.setPrefSize(100, 30);
+        btn3.setPrefSize(50, 30);
+
+        btn1.setDisable(true);
+        btn3.setDisable(true);
+
+        textField.setPrefWidth(400);
+        textField.setPrefHeight(30);
+        textField1.setPrefWidth(150);
+        textField1.setText("115.22.10.42");
+        textField2.setPrefWidth(150);
+        textField2.setText("5001");
+
+        textArea.setPadding(new Insets(10));
+
+        label1.setText("서버 주소 :");
+        label2.setText("포트 번호 :");
+        label3.setText("닉네임 입력:");
+
+        hBox1.setAlignment(Pos.CENTER);
+        hBox1.setSpacing(10);
+        hBox2.setSpacing(15);
+        hBox3.setAlignment(Pos.CENTER);
+        hBox3.setSpacing(15);
+        hBox4.setSpacing(10);
+        hBox4.setAlignment(Pos.CENTER);
+        //-------------------------------------
+
+        // 이 영역에 모든 코드가 들어감
+        // 방법 2
+        hBox2.getChildren().addAll(textField, btn3);//입력-전송창
+        hBox3.getChildren().addAll(label1, textField1, label2, textField2); //서버주소, 포트번호
+        hBox4.getChildren().addAll(label3, textField3, btn1, btn2);// 닉네임창
+        root.getChildren().addAll(hBox3, hBox4, textArea, hBox2);
+
+
+        textField3.setOnKeyTyped(keyEvent -> btn1.setDisable(false));
+
+
+        btn1.setOnAction(actionEvent -> {
+            String nickName = textField3.getText();
+            cs = new Socket();
+            try {
+                String serverAddress = textField1.getText();
+                int serverPort = Integer.parseInt(textField2.getText());
+                cs.connect(new InetSocketAddress(serverAddress, serverPort));
+                OutputStream outputStream = cs.getOutputStream();
+                byte[] data = nickName.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(data);
+            } catch (Exception e) {
+                textArea.setText(e.toString());
+                e.printStackTrace();
+            }
+
+            new ChatThread2(cs, this, nickName).start();
+            btn3.setDisable(false);
+        });
+
+        btn2.setOnAction(actionEvent -> System.exit(0));
+
+        textField.setOnAction(actionEvent -> {
+            try {
+                OutputStream outputStream = cs.getOutputStream();
+                String s = textField.getText();
+                byte[] data = s.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(data);
+                textField.setText("");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        btn3.setOnAction(actionEvent -> {
+            try {
+                OutputStream outputStream = cs.getOutputStream();
+                String s = textField.getText();
+                byte[] data = s.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(data);
+                textField.setText("");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        //------------------------------------
+        Scene scene = new Scene(root);
+        arg0.setScene(scene); // 위의 설정값들을 적용
+        arg0.setTitle("Client"); // 제목
+        arg0.show(); // 창 띄움
+
+    }
+}
+
+
+
+/*
+//ex75 UI 에서 통신
+class ConnectThread2 extends Thread{
+	@Override
+	public void run() {
+		try {
+			Socket cs = new Socket();
+
+			//cs.connect(new InetSocketAddress("localhost", 5001));
+			cs.connect(new InetSocketAddress("220.119.22.165", 5001));
+			System.out.println("접속 완료");
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+public class Client extends Application{
+	Socket cs;
+	@Override
+	public void start(Stage arg0) throws Exception {
+		VBox root = new VBox();
+		root.setPrefSize(400, 300); // 창 크기
+		root.setSpacing(15);
+		//-------------------------------------
+		// 이 영역에 모든 코드가 들어감
+		// 방법 2
+		Button btn1 = new Button("서버에 연결");
+		Button btn2 = new Button("버튼2");
+
+		root.getChildren().addAll(btn1, btn2);
+
+		btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				cs = new Socket();
+
+				try {
+					//cs.connect(new InetSocketAddress("localhost", 5001));
+					cs.connect(new InetSocketAddress("220.119.22.165", 5001));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		btn2.setOnAction(new EventHandler<ActionEvent>() {
+			int count = 0;
+			@Override
+			public void handle(ActionEvent arg0) {
+				try {
+					OutputStream outputStream = cs.getOutputStream();
+					String s ="apple" + count++;
+					byte[] data = s.getBytes();
+					outputStream.write(data);
+					System.out.println("데이터 보냄");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		//-------------------------------------
+		Scene scene = new Scene(root);
+		arg0.setScene(scene); // 위의 설정값들을 적용
+		arg0.setTitle("Client"); // 제목
+		arg0.show(); // 창 띄움
+
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+}
+*/
+
+
+/*
+//ex 74 server - client 통신
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("Client Start");
+
+
+		try {
+			Socket cs = new Socket();
+
+			System.out.println("숫자를 입력하면 접속을 시도합니다.");
+			new Scanner(System.in).nextInt();
+			// cs.connect(new InetSocketAddress("localhost", 5001));
+			cs.connect(new InetSocketAddress("220.119.22.165", 5001));
+			System.out.println("접속 완료");
+			System.out.println("숫자를 입력하면 데이터를 전송합니다.");
+			new Scanner(System.in).nextInt();
+
+			OutputStream outputStream = cs.getOutputStream();
+			String s ="apple";
+			byte[] data = s.getBytes();
+			outputStream.write(data);
+			System.out.println("데이터 보냄");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+
+		new Scanner(System.in).nextInt();
+		System.out.println("Client End");
+	}
+
+}
+*/
